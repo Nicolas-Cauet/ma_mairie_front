cd0c5b9b90d64e59bdc512bc75ab388fc2c8bf7e
baptiste-boimard <bouketin27@gmail.com>
2022-05-07 16:14:09 +0200

    Add button to menu with semantic ui

--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -11,82 +11,64 @@ function Menu() {
   const { isOpen } = useSelector((state) => state.menu);
 
   return (
-    <>
-    {/* <div className="div-grid">
-    <Grid columns={2}>
-    <Grid.Row>
-      <Grid.Column>
-        <Button className="grid-button">
-        <div className='div-icon'>
-          <Icon className="grid-icon" size="huge" name='world' />
-          <p>Coucou</p>
-        </div>
-        </Button>
-      </Grid.Column>
-      <Grid.Column>
-      <Button className="grid-button" primary>Primary
-          <Icon name='world' />
-        </Button>
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row>
-      <Grid.Column>
-      <Button className="grid-button" primary>Primary
-          <Icon name='world' />
-        </Button>
-      </Grid.Column>
-      <Grid.Column>
-      <Button className="grid-button" primary>Primary
-          <Icon name='world' />
-        </Button>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-    </div> */}
-
     <div className='menu'>
     { isOpen && (
-      <nav>
-      <div className='menu-list'>
-      
+      <nav className='menu-list'>
         <NavLink className='menu-item' to="/articles/:mairie_id">
-            <Button icon color='blue'>
-              <Icon name='newspaper outline' size='huge'/>
-              <p className='menu-label'>Articles</p>
-            </Button>
+          <Button color='blue'>
+            <section className='button-container'>
+              <Icon name='newspaper outline' />
+              <p>Articles</p>
+            </section>
+          </Button>
         </NavLink>
         <NavLink className='menu-item' to="/reports/:mairie_id">
-        <Button icon color='green' className='button-signaler'>
-              <Icon floated='right' name='warning sign' size='huge'/>
-              <p className='menu-label'>Signaler</p>
-            </Button>
+          <Button color='orange'>
+            <section className="button-container">
+              <Icon name='warning sign' />
+              <p>Signaler</p>
+            </section>
+          </Button>
         </NavLink>
-        <NavLink className='menu-item' to="/reports/:mairie_id">
-            <Icon name='warning sign' size='huge'/>
-            <p className='menu-label'>Signaler</p>
-        </NavLink>
-
         <NavLink className='menu-item' to="/council/:mairie_id">
-            <Icon name='building outline' size='huge'/>
-            <p className='menu-label'>Conseil municipal</p>
+          <Button color='red' className='council'>
+            <section className="button-container">
+              <Icon name='building outline' />
+              <p>Conseil Municipal</p>
+            </section>
+          </Button>
         </NavLink>
-
         <NavLink className='menu-item' to="/school/:mairie_id">
-            <Icon name='student' size='huge'/>
-            <p className='menu-label'>Ecoles</p>
+          <Button color='violet'>
+            <section className="button-container">
+              <Icon name='student' />
+              <p>Ecole</p>
+            </section>
+          </Button>
         </NavLink>
 
-        <NavLink className='menu-item menu-item--test' to="/admin">
-            <Icon name='sign-in' size='huge'/>
-            <p className='menu-label'>Espace administration</p>
-        </NavLink>
-
-      </div>
+          {/* <Button color='linkedin'>
+            <section className="button-container">
+              <Icon name='sign-in' />
+              <p>LinkedIn</p>
+            </section>
+          </Button>
+          <Button color='instagram'>
+            <section className="button-container">
+              <Icon name='instagram' />
+              <p>Instagram</p>
+            </section>
+          </Button>
+          <Button color='youtube'>
+              <section className="button-container">
+                <Icon name='youtube' />
+                <p>YouTube</p>
+              </section>
+          </Button> */}
+        
       </nav>
     )}
     </div>
-    </>
   );
 }
 
