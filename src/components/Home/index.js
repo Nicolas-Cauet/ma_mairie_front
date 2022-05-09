f9967a7121cca7129e454eae309e766893281c85
Benjamin-Pontoizeau <benjamin.pontoizeau@gmail.com>
2022-05-09 14:26:49 +0200

    add useEffect in home for menu view

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { toggleMenu } from '../../actions/menu';
 import Infos from '../Infos';
 import './style.scss';
 
 function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(toggleMenu(true));
+
+    return function cleanup() {
+      dispatch(toggleMenu(false))
+    }
+  },);
+
   return (
     <div className='home'>
         <p>Bienvenue sur la home page</p>
