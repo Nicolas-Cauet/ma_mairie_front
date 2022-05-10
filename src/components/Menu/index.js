e0338754e171d455ed6c484b304170910c9a4555
baptiste-boimard <bouketin27@gmail.com>
2022-05-10 12:36:53 +0200

    Adjustment css Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,7 @@
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import React from 'react';
-import { Button, Icon,} from 'semantic-ui-react'
+import { Button, Icon, } from 'semantic-ui-react'
 
 import './style.scss';
 
@@ -11,49 +11,57 @@ function Menu() {
   const { isOpen } = useSelector((state) => state.menu);
 
   return (
-    <>
-      <div className='menu'>
-      { isOpen && (
-        <nav>
-          <div className='menu-list'>
-            <NavLink className='menu-item' to="/articles/:mairie_id">
-                <Button icon color='blue'>
-                  <Icon name='newspaper outline' size='huge'/>
-                  <p className='menu-label'>Articles</p>
-                </Button>
-            </NavLink>
-            <NavLink className='menu-item' to="/reports/:mairie_id">
-            <Button icon color='green' className='button-signaler'>
-                  <Icon floated='right' name='warning sign' size='huge'/>
-                  <p className='menu-label'>Signaler</p>
-                </Button>
-            </NavLink>
-            <NavLink className='menu-item' to="/reports/:mairie_id">
-                <Icon name='warning sign' size='huge'/>
-                <p className='menu-label'>Signaler</p>
-            </NavLink>
-            <NavLink className='menu-item' to="/council/:mairie_id">
-                <Icon name='building outline' size='huge'/>
-                <p className='menu-label'>Conseil municipal</p>
-            </NavLink>
-            <NavLink className='menu-item' to="/school/:mairie_id">
-                <Icon name='student' size='huge'/>
-                <p className='menu-label'>Ecoles</p>
-            </NavLink>
-            <NavLink className='menu-item menu-item--test' to="/admin">
-                <Icon name='sign-in' size='huge'/>
-                <p className='menu-label'>Espace administration</p>
-            </NavLink>
-          </div>
-          </nav>
-        )}
-        </div>
-    </>
+    <div className='menu'>
+    { isOpen && (
+      <nav className='menu-list'>
+        <NavLink className='menu-item' to="/articles/:mairie_id">
+          <Button className='article'>
+            <section className='buttonMenu-container'>
+              <Icon name='newspaper outline' />
+              <p>Articles</p>
+            </section>
+          </Button>
+        </NavLink>
+        <NavLink className='menu-item' to="/reports/:mairie_id">
+          <Button className='warning'>
+            <section className="buttonMenu-container">
+              <Icon name='warning sign' />
+              <p>Signaler</p>
+            </section>
+          </Button>
+        </NavLink>
+        <NavLink className='menu-item' to="/council/:mairie_id">
+          <Button className='council'>
+            <section className="buttonMenu-container">
+              <Icon name='building outline' />
+              <p>Conseil Municipal</p>
+            </section>
+          </Button>
+        </NavLink>
+        <NavLink className='menu-item' to="/school/:mairie_id">
+          <Button className='school'>
+            <section className="buttonMenu-container">
+              <Icon name='student' />
+              <p>Ecole</p>
+            </section>
+          </Button>
+        </NavLink> 
+        <NavLink className='menu-item' to="/admin">
+          <Button className='admin'>
+            <section className="buttonMenu-container">
+              <Icon name='sign-in' />
+              <p>Administration</p>
+            </section>
+          </Button>
+        </NavLink>       
+      </nav>
+    )}
+    </div>
   );
 }
 
-Menu.propTypes = {
+// Menu.propTypes = {
 
-};
+// };
 
 export default Menu;
