607c9f134b8aa7ab9b7208f404b3f3e35f0bb8fb
Benjamin-Pontoizeau <benjamin.pontoizeau@gmail.com>
2022-05-11 15:41:29 +0200

    hide menu after redirect

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,28 @@
 // import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import { Button, Icon, } from 'semantic-ui-react'
 
 import './style.scss';
+import { toggleMenu } from '../../actions/menu';
 
 function Menu() {
 
   const { isOpen } = useSelector((state) => state.menu);
 
+  const dispatch= useDispatch();
+
+  //After redirect, hide menu on click
+  const hideMenu = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className='menu'>
     { isOpen && (
       <nav className='menu-list'>
-        <NavLink className='menu-item' to="/articles/:mairie_id">
+        <NavLink className='menu-item' to="/articles/:mairie_id" onClick={hideMenu}>
           <Button className='article'>
             <section className='buttonMenu-container'>
               <Icon name='newspaper outline' />
@@ -22,7 +30,7 @@ function Menu() {
             </section>
           </Button>
         </NavLink>
-        <NavLink className='menu-item' to="/reports">
+        <NavLink className='menu-item' to="/reports" onClick={hideMenu}>
           <Button className='warning'>
             <section className="buttonMenu-container">
               <Icon name='warning sign' />
@@ -30,7 +38,7 @@ function Menu() {
             </section>
           </Button>
         </NavLink>
-        <NavLink className='menu-item' to="/council/:mairie_id">
+        <NavLink className='menu-item' to="/council/:mairie_id" onClick={hideMenu}>
           <Button className='council'>
             <section className="buttonMenu-container">
               <Icon className='council' name='building outline' />
@@ -38,7 +46,7 @@ function Menu() {
             </section>
           </Button>
         </NavLink>
-        <NavLink className='menu-item' to="/school/:mairie_id">
+        <NavLink className='menu-item' to="/school/:mairie_id" onClick={hideMenu}>
           <Button className='school'>
             <section className="buttonMenu-container">
               <Icon name='student' />
@@ -46,7 +54,7 @@ function Menu() {
             </section>
           </Button>
         </NavLink> 
-        <NavLink className='menu-item' to="/admin">
+        <NavLink className='menu-item' to="/admin" onClick={hideMenu}>
           <Button className='admin'>
             <section className="buttonMenu-container">
               <Icon name='sign-in' />
