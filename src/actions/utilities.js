320caae4fa0397a3b99ff392336a4278dce5e46c
baptiste-boimard <bouketin27@gmail.com>
2022-05-12 17:16:34 +0200

    Add comments for utilities action

--- src/actions/utilities.js
+++ src/actions/utilities.js
@@ -1,9 +1,11 @@
+//Management to redirect route
 export const REDIRECT = 'REDIRECT';
 export const redirect = (route) => ({
   type: REDIRECT,
   redirectTo: route,
 });
 
+//Controlled field for Field component
 export const CHANGE_CURRENT_FIELD = 'CHANGE_CURRENT_FIELD';
 export const changeCurrentField = (value, key) => ({
   type: CHANGE_CURRENT_FIELD,
