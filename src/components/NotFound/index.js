b7e15589322146bfc2290c5f04d63b250a86035c
Benjamin-Pontoizeau <benjamin.pontoizeau@gmail.com>
2022-05-21 13:55:50 +0200

    add message for 404 error

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -12,15 +12,33 @@ function NotFound() {
     navigate('/admin');
   };
 
+  const redirectToHome = () => {
+    navigate('/');
+  };
+
   return (
     <div className="notfound">
-      { (window.location.pathname.includes('admin') && !logged) && (
+      { (window.location.pathname.includes('admin') && !logged) ? (
         <div className="notfound-container">
           <Message negative className="notfound-message">
             Vous devez être connecté pour accèder à cette page
           </Message>
           <Button onClick={redirectToLogin} className="notfound-button">
             Se connecter
+          </Button>
+        </div>
+      ) : (
+        <div className="notfound-container">
+          <Message className="notfound-message">
+            <Message.Header>
+              Erreur 404
+            </Message.Header>
+            <p>
+              La page que vous recherchez semble introuvable
+            </p>
+          </Message>
+          <Button onClick={redirectToHome} className="notfound-button">
+            Retour à l'accueil
           </Button>
         </div>
       ) }
