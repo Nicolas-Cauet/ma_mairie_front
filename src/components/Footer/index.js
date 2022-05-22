69a33d15c77bc076f2cad90b9745f631d1fc7fa2
Benjamin-Pontoizeau <benjamin.pontoizeau@gmail.com>
2022-05-22 12:21:21 +0200

    responsive desktop footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,8 @@ import './style.scss';
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <div className="footer">
+    <div className="footer">
+      <div className="footer-content">
         <div className="footer-infos">
           <h2>Informations de la mairie</h2>
           <p>Adresse de la mairie :<br />
@@ -17,21 +17,23 @@ function Footer() {
           <p>Téléphone : 01 02 03 04 05</p>
           <p>Email : mairie@mairie.fr</p>
         </div>
-        <div className="footer-hourly">
-          <h2>Horaires de la mairie</h2>
-          <p>Du lundi au mardi de 9h à 12h<br />
-            Puis du jeudi au vendredi de 14h à 17h
-          </p>
-        </div>
-        <div className="footer-followUs">
-          <h2>Suivez-nous</h2>
-          <div className="footer-followUs-icons">
-            <Button circular color="facebook" icon="facebook" />
-            <Button circular color="twitter" icon="twitter" />
+        <div className="footer-rightContent">
+          <div className="footer-hourly">
+            <h2>Horaires de la mairie</h2>
+            <p>Du lundi au mardi de 9h à 12h<br />
+              Puis du jeudi au vendredi de 14h à 17h
+            </p>
+            <div className="footer-followUs">
+              <h2>Suivez-nous</h2>
+              <div className="footer-followUs-icons">
+                <Button circular color="facebook" icon="facebook" />
+                <Button circular color="twitter" icon="twitter" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="copyright">© mamairie.fr @2022</div>
       </div>
+      <div className="copyright">© mamairie.fr @2022</div>
     </div>
   );
 }
